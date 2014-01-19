293a2e242732235036097fb935d3f40d9cc6fdaa
codng <softwaresoa@gmail.com>
2014-01-19 13:59:15 -0600

    'follow'

--- src/main/java/com/sable/cb/web/OrganizationController.java
+++ src/main/java/com/sable/cb/web/OrganizationController.java
@@ -1,18 +1,20 @@
 package com.sable.cb.web;
 
-import com.sable.cb.domain.Organization;
-import com.sable.cb.domain.Post;
-import com.sable.cb.service.OrganizationService;
-import com.sable.cb.service.PostType;
-
-
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -26,7 +28,10 @@ import org.springframework.web.util.UriUtils;
 import org.springframework.web.util.WebUtils;
 
 import com.sable.cb.domain.Organization;
+import com.sable.cb.domain.Users;
+import com.sable.cb.repository.UsersRepository;
 import com.sable.cb.service.OrganizationService;
+import com.sable.cb.service.UsersService;
 
 @RequestMapping("/organizations")
 @Controller
@@ -35,15 +40,87 @@ public class OrganizationController {
 	@Autowired
     OrganizationService organizationService;
 	
-
-	@RequestMapping(value ="/getByLoc", method = RequestMethod.	POST)
-	@ResponseBody()
-	public List getAllOrganization()
-	{
-		return organizationService.findAllOrganizations();
-	}
+	@Autowired
+    UsersService userService;
+	@Autowired
+	UsersRepository usersRepository;
 	
 
+	@RequestMapping(value ="/getByLoc", method = RequestMethod.GET, produces = "application/json")
+	@ResponseBody
+	public Map<String, Object> getAllOrganization()
+	{
+		Map<String, Object> response = new HashMap<String, Object>();
+		String user = "mohdhamedmscse@gmail.com";//SecurityContextHolder.getContext().getAuthentication().getName();
+        Users users = usersRepository.findByEmail(user);
+        Set<Organization> followedOrgs = users.getFollowedOrgs();
+        List allOrgs = organizationService.findAllOrganizations();
+        List<Organization> followedOrganizations = getMappedOrganizations(followedOrgs);
+        List<Organization> nonFollowedOrganizations = getMappedOrganizations((List) CollectionUtils.subtract(allOrgs, followedOrgs));
+        
+        response.put("followedOrgs", followedOrganizations);
+        response.put("nonFollowedOrgs",nonFollowedOrganizations);
+        return response;
+	}
+	
+	private List<Organization> getMappedOrganizations(Collection<Organization> orgs) {
+		List<Organization> mappedOrganizations = new ArrayList<Organization>();
+		if (CollectionUtils.isNotEmpty(orgs)) {
+			for (Organization organization : orgs) {
+				Organization org = new Organization();
+				org.setId(organization.getId());
+				org.setName(organization.getName());
+				org.setCity(organization.getCity());
+				mappedOrganizations.add(org);
+			}
+		}
+		return mappedOrganizations;
+	}
+	
+	
+	
+	
+	@RequestMapping(value ="/follow", method = RequestMethod.POST, produces = "application/json") 	
+	@ResponseBody
+	public Map<String, Object> follow(@RequestBody List<Organization> myArray)
+	{
+		Map<String, Object> response = new HashMap<String, Object>();
+		String user =SecurityContextHolder.getContext().getAuthentication().getName();// "mohdhamedmscse@gmail.com";//
+        Users users = usersRepository.findByEmail(user);
+             //  1,2  - 2,3  = 1  1,2,3
+        //organizationService.deleteOrganization(organization)
+         Set<Organization> finalUpdate = new HashSet<Organization>();
+         for(Organization eachOrg : myArray){
+        	Organization updateOrganization = organizationService.findOrganization(eachOrg.getId());
+        	finalUpdate.add(updateOrganization);
+        }
+         users.setFollowedOrgs(finalUpdate);
+         users = userService.updateUsers(users);
+         
+         List allOrgs = organizationService.findAllOrganizations();
+         List<Organization> followedOrganizations = getMappedOrganizations(users.getFollowedOrgs());
+         List<Organization> nonFollowedOrganizations = getMappedOrganizations((List) CollectionUtils.subtract(allOrgs, users.getFollowedOrgs()));
+         
+         response.put("followedOrgs", followedOrganizations);
+         response.put("nonFollowedOrgs",nonFollowedOrganizations);
+         return response;
+	}
+	
+//	private List<Organization> getUniqueOrganizations(List<Organization> orgList1, List<Organization> orgList2) {
+//		List<Organization> uniqueOrgs = new ArrayList<Organization>();
+//		if (CollectionUtils.isEmpty(orgList1))
+//			return uniqueOrgs;
+//		else {
+//			if (CollectionUtils.isEmpty(orgList2))
+//				return orgList1;
+//			else {
+//				Map<Integer, Boolean> containsMap = new HashMap<Integer, Boolean>
+//			}
+//		}
+//	}
+	
+	
+	
 	@RequestMapping(method = RequestMethod.POST, produces = "text/html")
     public String create(@Valid Organization organization, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
         if (bindingResult.hasErrors()) {
