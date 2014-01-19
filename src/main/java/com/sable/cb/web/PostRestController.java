c01a80242b8c35ec088a11477266c7a7ae7dab6c
Intesar Mohammed <mdshannan@gmail.com>
2014-01-18 22:52:51 -0800

    added decoding

--- src/main/java/com/sable/cb/web/PostRestController.java
+++ src/main/java/com/sable/cb/web/PostRestController.java
@@ -1,4 +1,9 @@
 package com.sable.cb.web;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,13 +20,20 @@ import com.sable.cb.service.PostType;
 @Controller
 public class PostRestController {
 
+	Logger logger = Logger.getLogger(getClass());
+	
 	@Autowired
     PostService postService;
 
 	@RequestMapping(value="/post/rest", method = RequestMethod.POST)
 	@ResponseBody
 	public void create(@RequestBody String content) {
-		
+		try {
+			content = URLDecoder.decode(content, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		logger.info("content = " + content);
 		Post post = new Post();
 		post.setContent(content);
 		post.setPostType(PostType.GENERAL.toString());
