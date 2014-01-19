2966180e7ee0fb5a9a319ab5498f068263db5638
codng <softwaresoa@gmail.com>
2014-01-18 21:15:43 -0600

    Added getAllOrganization.

--- src/main/java/com/sable/cb/web/OrganizationController.java
+++ src/main/java/com/sable/cb/web/OrganizationController.java
@@ -1,9 +1,10 @@
 package com.sable.cb.web;
-import com.sable.cb.domain.Organization;
-import com.sable.cb.service.OrganizationService;
 import java.io.UnsupportedEncodingException;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,8 +13,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.util.UriUtils;
 import org.springframework.web.util.WebUtils;
+
+import com.sable.cb.domain.Organization;
+import com.sable.cb.service.OrganizationService;
 
 @RequestMapping("/organizations")
 @Controller
@@ -21,6 +26,14 @@ public class OrganizationController {
 
 	@Autowired
     OrganizationService organizationService;
+	
+	@RequestMapping(value ="/getByLoc", method = RequestMethod.	POST)
+	@ResponseBody()
+	public List getAllOrganization()
+	{
+		return organizationService.findAllOrganizations();
+	}
+	
 
 	@RequestMapping(method = RequestMethod.POST, produces = "text/html")
     public String create(@Valid Organization organization, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
